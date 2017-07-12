819baa7e35f9133c846387fc8d2e8635362f6e7b
christopher laird <fclaird@yahoo.com>
2017-07-13 05:56:04 +0900

    complete 17 and test drive debug in vsCode

--- ex17.py
+++ ex17.py
@@ -1,31 +1,30 @@
-
 from os.path import exists
 
-from_file, to_file = sample.txt, 
+from_file, to_file = "sample.txt", "test.txt"
 
-print "Copying from %s to %s" % (from_file, to_file)
+print "Copying from %s to %s\n" % (from_file, to_file)
 
 in_file = open(from_file)
 inData = in_file.read()
 
-print "The input file is %d bytes long" % len(inData)
+print "The input file is %d bytes long\n" % len(inData)
 
+print "Does the output file exist?  %r\n" % exists(to_file)
+print "Ready, hit RETURN to continue, or CTRL-C to abort.\n"
 
-print "Does the output file exist?  %r" % exists(to_file)
-print "Ready, hit RETURN to continue, or CTRL-C to abort."
 raw_input()
 
 out_file = open(to_file, 'w')
 out_file.write(inData)
 out_file.close()
 
-print "complete!"
+print "complete!\n"
 
-check = open(out_file, 'r')
-print check.read()
+check = open(to_file, 'r')
+print check.read() + "\n"
 print "\n"
 
-print in_file.read()
+print in_file.read() + "\n"
 
 
 out_file.close()
